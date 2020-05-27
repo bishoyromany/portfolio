6d15609f5c013da846ed9f1ed8cc8e2f7526c080
Bishoy Romany <7bishoyromany7@gmail.com>
2020-05-27 13:04:32 +0200

    Added Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './../Style/Navbar.scss'
+import {connect} from 'react-redux'
+import store from '../Store/store';
 
-const Navbar = () => {
+const Navbar = ({NAVBAR_URLS}) => {
+    const [showNavbar, setShowNavbar] = useState(false);
     return(
-        <header id="navbar">
-            <div className="list-icon">
+        <header id="navbar" className={`${showNavbar ? 'active' : ''}`}>
+            <div className="list-icon" onClick={() => setShowNavbar(!showNavbar)}>
                 <span></span>
                 <span></span>
                 <span></span>
+            </div>
+            
+            <div className="links">
+                <span className="background"></span>
+                <ul>
+                    {
+                        NAVBAR_URLS.map((item) => {
+                            return(
+                                <li title={item.title} key={item.url}>
+                                    <a href={item.url}>
+                                        <img src={item.icon} />
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         </header>
     );  
 };
 
-export default Navbar;
+const mapStateToProps = (state /*, ownProps*/) => {
+    return {
+        NAVBAR_URLS : state.NAVBAR_URLS,
+    }
+}
+  
+
+
+// const mapDispatchToProps = { increment, decrement, reset }
+
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions : {
+
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,46 @@
 import { createStore } from 'redux'
 
+import homepage from './../Images/homepage.svg'
+import skills from './../Images/skills.svg'
+import portfolio from './../Images/websites.svg'
+import resume from './../Images/resume.svg'
+import hire from './../Images/hire.svg'
+
 let data = {
     APP_NAME : "Portfolio",
     BASE_URL : "/",
+    NAVBAR_URLS: [
+        {
+            title : 'Home',
+            url: '/#Home',
+            class: 'home-url',
+            icon : homepage,
+        },
+        {
+            title : 'Skills',
+            url: '/#Skills',
+            class: 'skills-url',
+            icon : skills,
+        },
+        {
+            title : 'Portfolio',
+            url: '/#Portfolio',
+            class: 'portfolio-url',
+            icon : portfolio,
+        },
+        {
+            title : 'Resume',
+            url: '/#Resume',
+            class: 'resume-url',
+            icon : resume,
+        },
+        {
+            title : 'Hire Me',
+            url: '/#HireMe',
+            class: 'hire-me-url',
+            icon : hire,
+        },
+    ],
 };
 
 const storeData = (state = data, action) => {
